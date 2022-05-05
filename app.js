b80e3453c9b79e485220621b9b78e5866aa9fbe7
Val10 <valentindragnev101@gmail.com>
2022-05-05 07:14:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
+//because database is on our local drive I will not use fetch or async functions.
+//for iterating over the database it will be used , for loop.
+
 for (let i = 0; i < storeProducts.length; i++) {
     const body = document.body;
+
+    //creating all the elements that will contain the values of the properties from DB
     const div = document.createElement("div");
-
-
     const pName = document.createElement('p');
     const pPrice = document.createElement('p');
     const pQty = document.createElement('p');
     const pDate = document.createElement('p');
 
+    //taking the values from DB
     const productName = storeProducts[i].ProductName;
     const unitPrice = storeProducts[i].UnitPrice;
     const qty = storeProducts[i].UnitsInStock;
     const date = storeProducts[i].DeliveryOn;
 
-    // Check for values in qty and if there is 0 value, add style , color red 
+    // Check for values in qty and if there is 0 value, add style , color red.
     if (qty === 0) {
-        pQty.style.color = "red"
-        pQty.classList.add("notAvailableItems")
-
-
+        pQty.style.color = "red";
+        pQty.classList.add("notAvailableItems");
     } else {
-        pQty.style.color = "none"
-        pQty.classList.add("availabelItems")
+        pQty.style.color = "none";
+        pQty.classList.add("availabelItems");
 
     }
 
+    //pushing the taken values to the HTML elements nd concatinate with description to be showed.
     pName.innerText = "Product name: " + productName;
     pPrice.innerText = "Price: " + unitPrice + "$";
     pQty.innerText = "Quantity: " + qty + "pcs.";
     pDate.innerText = "Delivered on: " + date;
 
+    //creating classes for every element for easy styling with CSS or SASS .
     div.append(pName);
     pName.classList.add("itemName");
 
@@ -41,7 +45,7 @@ for (let i = 0; i < storeProducts.length; i++) {
     div.append(pDate);
     pDate.classList.add("date")
 
-
+    //append all the elements to the DOM. 
     body.append(div)
 
 
